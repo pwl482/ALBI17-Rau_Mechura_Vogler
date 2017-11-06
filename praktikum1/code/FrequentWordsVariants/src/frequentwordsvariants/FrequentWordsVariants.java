8d6e66c7d0fdbe83f5fa66a1b32b5c6603fdae7c
Extinktor482 <Extinktor482@users.noreply.github.com>
2017-11-06 14:57:02 +0100

    Add files via upload

---
+++ praktikum1/code/FrequentWordsVariants/src/frequentwordsvariants/FrequentWordsVariants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frequentwordsvariants;
+
+/**
+ *
+ * @author Paul Vogler & Tobias Mechura
+ */
+public class FrequentWordsVariants {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
